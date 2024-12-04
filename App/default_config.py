a9c7a8631dd3a1c94e50e4878ad43358fd7d7ec1
zach <89186931+ZachryRamkissoon@users.noreply.github.com>
2024-12-04 11:29:56 -0400

    Update default_config.py
    Signed-off-by: zach <89186931+ZachryRamkissoon@users.noreply.github.com>

--- App/default_config.py
+++ App/default_config.py
@@ -1,2 +1,3 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+# SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgresql://comp3613_project_user:r0S2zoKTmfzCUqvHDGjFtaDFnrVoJDkL@dpg-ct875mggph6c73d34uv0-a.oregon-postgres.render.com/comp3613_project"
 SECRET_KEY = "secret key"
