ef3d925aac63e09e7b4b4840c330096bbc64f1af
Rikuthin <125291285+Zurun-Rikuthin@users.noreply.github.com>
2025-02-10 11:02:16 -0400

    Refactored Listing model into JobListing.
    Updated config.py and default_config.py to store valid values for JobListing.admin_approval_status and JobListing.position_types

--- App/config.py
+++ App/config.py
@@ -5,14 +5,26 @@ def load_config(app, overrides):
         app.config.from_object('App.custom_config')
     else:
         app.config.from_object('App.default_config')
+    
+    # Load environment variable-based configurations
     app.config.from_prefixed_env()
+
+    # Core settings
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['PREFERRED_URL_SCHEME'] = 'https'
     app.config['UPLOADED_PHOTOS_DEST'] = "App/uploads"
+
+    # JWT authentication settings
     app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token'
     app.config["JWT_TOKEN_LOCATION"] = ["cookies", "headers"]
     app.config["JWT_COOKIE_SECURE"] = True
     app.config["JWT_COOKIE_CSRF_PROTECT"] = False
+
+    # Dynamic definition of valid model attributes
+    app.config["JOB_POSITION_TYPES"] = set(app.config.get("JOB_POSITION_TYPES", []))
+    app.config["APPROVAL_STATUSES"] = set(app.config.get("APPROVAL_STATUSES", []))
+    
+
     for key in overrides:
         app.config[key] = overrides[key]
--- App/models/__init__.py
+++ App/models/__init__.py
@@ -2,7 +2,7 @@ from .admin_account import *
 from .alumni_account import *
 from .base_user_account import *
 from .company_account import *
-from .listing import *
+from .job_listing import *
 from .notification import *
 from .observer import *
 from .subject import *
